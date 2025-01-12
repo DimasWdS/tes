fb2c39e595b458ba605d05d35c513af658183c41
DimasWdS <dimaswidysaputra41@gmail.com>
2025-01-12 10:39:47 +0700

    first commit

---
+++ JAVASCRIP/index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const nn = document.getElementById("nn");
+btn.addEventListener("click", function () {
+  console.log("tes");
+  nn.classList.toggle("none");
+});
